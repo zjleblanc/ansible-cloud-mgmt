8c5c04e6884d3274ef389eb1b82c19f5e40c989c
Zachary LeBlanc <zjleblanc3@gmail.com>
2024-06-13 18:55:00 -0700

    init steps for converting EC2 metadata to CMDB items

--- demos/filter_plugins/aws_to_sn_obj.py
+++ demos/filter_plugins/aws_to_sn_obj.py
@@ -3,7 +3,9 @@ class FilterModule(object):
   def filters(self):
     return { 
       "to_sn_vpc": self.convert_to_sn_vpc,
-      "to_sn_subnet": self.convert_to_sn_subnet 
+      "to_sn_subnet": self.convert_to_sn_subnet,
+      "to_sn_security_group": self.convert_to_sn_security_group,
+      "to_sn_ec2": self.convert_to_sn_ec2
     }
 
   def convert_to_sn_vpc(self, vpc):
@@ -42,5 +44,70 @@ class FilterModule(object):
       "u_subnet_id": subnet["subnet_id"],
       "u_name": subnet["tags"]["Name"],
       "u_vpc_id": subnet["vpc_id"],
+      "u_vpc": vpc_sys_id
+    }
+
+  def convert_to_sn_security_group(self, sg, vpc_sys_id):
+    return {
+      "u_name": sg["group_name"],
+      "u_id": sg["group_id"],
+      "u_owner_id": sg["owner_id"],
+      "u_description": sg["description"],
+      "u_vpc": vpc_sys_id
+    }
+  
+  def convert_to_sn_ec2(self, instance, vpc_sys_id, subnet_sys_id):
+    return {
+      "u_ami_launch_index": instance["ami_launch_index"],
+      "u_architecture": instance["architecture"],
+      "u_boot_mode": instance["boot_mode"],
+      "u_capacity_reservation_preference": instance["capacity_reservation_specification"]["capacity_reservation_preference"],
+      # "u_client_token": instance["client_token"],
+      "u_core_count": instance["cpu_options"]["core_count"],
+      "u_threads_per_core": instance["cpu_options"]["threads_per_core"],
+      "u_current_instance_boot_mode": instance["current_instance_boot_mode"],
+      "u_ebs_optimized": instance["ebs_optimized"],
+      "u_ena_support": instance["ena_support"],
+      # "u_enabled": instance["enabled"],
+      # "u_configured": instance["configured"],
+      "u_hypervisor": instance["hypervisor"],
+      "u_image_id": instance["image_id"],
+      "u_instance_id": instance["instance_id"],
+      "u_instance_type": instance["instance_type"],
+      "u_key_name": instance["key_name"],
+      "u_launch_time": instance["launch_time"],
+      "u_auto_recovery": instance["maintenance_options"]["auto_recovery"],
+      # "u_http_endpoint": instance["http_endpoint"],
+      # "u_http_protocol_ipv6": instance["http_protocol_ipv6"],
+      # "u_http_put_response_hop_limit": instance["http_put_response_hop_limit"],
+      # "u_http_tokens": instance["http_tokens"],
+      # "u_instance_metadata_tags": instance["instance_metadata_tags"],
+      # "u_state": instance["state"],
+      # "u_state": instance["state"],
+      "u_availability_zone": instance["placement"]["availability_zone"],
+      "u_group_name": instance["placement"].get("group_name", None),
+      "u_tenancy": instance["placement"]["tenancy"],
+      "u_platform_details": instance["platform_details"],
+      "u_private_dns_name": instance["private_dns_name"],
+      "u_enable_resource_name_dns_a_record": instance["private_dns_name_options"]["enable_resource_name_dns_a_record"],
+      "u_enable_resource_name_dns_aaaa_record": instance["private_dns_name_options"]["enable_resource_name_dns_aaaa_record"],
+      "u_hostname_type": instance["private_dns_name_options"]["hostname_type"],
+      "u_private_ip_address": instance["private_ip_address"],
+      "u_public_dns_name": instance["public_dns_name"],
+      "u_root_device_name": instance["root_device_name"],
+      "u_root_device_type": instance["root_device_type"],
+      "u_source_dest_check": instance["source_dest_check"],
+      # "u_code": instance["code"],
+      # "u_name": instance["name"],
+      # "u_code": instance["code"],
+      # "u_message": instance["message"],
+      "u_state_transition_reason": instance["state_transition_reason"],
+      "u_subnet": subnet_sys_id,
+      "u_name": instance["tags"].get("Name", instance["instance_id"]),
+      "u_aws_ec2launchtemplate_id": instance["tags"].get("aws:ec2launchtemplate:id", "N/A"),
+      "u_aws_ec2launchtemplate_version": instance["tags"].get("aws:ec2launchtemplate:version", "N/A"),
+      "u_usage_operation": instance["usage_operation"],
+      "u_usage_operation_update_time": instance["usage_operation_update_time"],
+      "u_virtualization_type": instance["virtualization_type"],
       "u_vpc": vpc_sys_id
     }
