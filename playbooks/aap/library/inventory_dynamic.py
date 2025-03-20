2dcc62e98d61d7b852724665c720be361d03926d
Zachary LeBlanc <zjleblanc3@gmail.com>
2025-03-20 08:53:32 -0500

    update inventory export modules

--- playbooks/aap/library/inventory_dynamic.py
+++ playbooks/aap/library/inventory_dynamic.py
@@ -61,31 +61,40 @@ def __get_related(host, token, resource):
     names = map(lambda resource: resource['name'], res.json()['results'])
     return list(names)
 
-def __get_inventories(host, token):
-    url = "https://" + host + "/api/v2/inventories/"
+def __get_inventories(host, token, names, is_ge_25):
+    resource = "/api/controller/v2/inventories/" if is_ge_25 else "/api/v2/inventories/"
+    url = "https://" + host + resource
     res = requests.get(url, headers={"Authorization": "Bearer " + token})
     inventories_raw = res.json()['results']
-
+    
+    inventories_dynamic = []
     for inv in inventories_raw:
+        if names and inv['name'] not in names:
+            continue
         if inv['related'].get('instance_groups', None):
             inv['instance_groups'] = __get_related(host, token, inv['related']['instance_groups'])
         if inv['related'].get('input_inventories', None):
             inv['input_inventories'] = __get_related(host, token, inv['related']['input_inventories'])
+        inventories_dynamic.append(inv)
 
-    inventories = map(__map_to_cac, inventories_raw)
+    inventories = map(__map_to_cac, inventories_dynamic)
     return list(inventories)
 
 def main():
     argument_spec = dict(
         controller_host=dict(required=False, aliases=['tower_host'], fallback=(env_fallback, ['CONTROLLER_HOST', 'TOWER_HOST'])),
-        controller_oauthtoken=dict(required=False, aliases=['tower_username'], fallback=(env_fallback, ['CONTROLLER_OAUTH_TOKEN', 'TOWER_OAUTH_TOKEN'])),
+        controller_oauthtoken=dict(required=False, no_log=True, aliases=['tower_username'], fallback=(env_fallback, ['CONTROLLER_OAUTH_TOKEN', 'TOWER_OAUTH_TOKEN'])),
+        names=dict(required=False,type=list,default=None),
+        ge_25=dict(required=False,type=bool,default=False)
     )
 
     module = AnsibleModule(argument_spec=argument_spec)
 
     inventories = __get_inventories(
         module.params.get('controller_host'), 
-        module.params.get('controller_oauthtoken')
+        module.params.get('controller_oauthtoken'),
+        module.params.get('names'),
+        module.params.get('ge_25'),
     )
 
     # Run the export process
