189e71c9db52aa2cc2d8539b0168a1dcdd899b01
Zachary LeBlanc <zjleblanc3@gmail.com>
2025-05-28 12:39:56 -0500

    add script for selenium screen capture demo

---
+++ demos/scripts/selenium_screen_capture.py
@@ -0,0 +1,36 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+# run Chrome in headless mode
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+
+# start a driver instance
+driver = webdriver.Chrome(options=options)
+
+# set the browser window size
+driver.set_window_size(1500, 1000)
+
+# Open the Python website
+driver.get("https://www.python.org")
+driver.implicitly_wait(2)
+
+# Print the page title
+print(driver.title)
+
+# Find the search bar using its name attribute
+search_bar = driver.find_element(By.NAME, "q")
+search_bar.clear()
+search_bar.send_keys("getting started with python")
+search_button = driver.find_element(By.ID, "submit")
+search_button.click()
+
+# Print the current URL
+full_body_element = driver.find_element(By.TAG_NAME, "body")
+full_body_element.screenshot(os.environ.get('SCREENSHOTS_DIR') + '/python_org_search.png')
+
+# Close the browser window
+driver.close()
